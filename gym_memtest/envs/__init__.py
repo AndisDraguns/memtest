a1eb7ef26c9c343a5711324e5ce2e07f3eca8c7f
Andis Draguns <andis.draguns@gmail.com>
2019-05-06 02:00:11 +0300

    Add files via upload
    Adding memtest files

---
+++ gym_memtest/envs/__init__.py
@@ -0,0 +1,1 @@
+from gym_memtest.envs.memtest_env import MemTestEnv
---
+++ gym_memtest/envs/memtest_env.py
@@ -0,0 +1,43 @@
+import gym
+from gym import error, spaces, utils
+from gym.utils import seeding
+import random
+import numpy as np
+ 
+class BubbleShooterEnv(gym.Env):  
+    metadata = {'render.modes': ['human']}   
+    def __init__(self):
+    	self.time = 0
+    	self.max_time = 100
+    	self.cell_history = [-1]*self.max_time
+
+    	self.state = None
+        self.action_space = 2
+        self.observation_space = 1
+ 
+    def step(self, action):
+        time = self.time
+        cell_history = self.cell_history
+
+        if(cell_history[time - 1] == action): # last action guessed
+        	reward = 1.0
+        else:
+        	reward = -1.0
+
+        if(time < self.max_time):
+        	self.state = random.randint(0,2) # 0 or 1
+        	cell_history[time] = self.state
+        	self.time += 1
+        	done = False
+ 		else:
+ 			done = True
+
+ 		return np.array(self.state), reward, done, {}
+
+    def reset(self):
+        self.state = random.randint(0,2)
+        self.time = 0
+        return np.array(self.state)
+ 
+    def render(self, mode='human', close=False):
+        pass
